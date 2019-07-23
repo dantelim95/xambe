639954efc58eb1ffa15ab2797371a5366cb93e17
limkc <keechian@gmail.com>
2019-07-23 10:00:26 +0800

    - init, category tree

---
+++ app/Http/Controllers/Api/ContentController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Category;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+class ContentController extends Controller
+{
+    //
+    private function findChildren($category) {
+        $ret = array();
+
+        $children = $category->children()->get();
+        foreach($children as $child) {
+            $ret[$child->category] = $child->toArray();
+            $ret[$child->category]['children'] = $this->findChildren($child);
+        }
+        return $ret;
+    }
+    public function getCategory(Request $request) {
+
+        // DB::connection()->enableQueryLog();
+        $categories = Category::orderBy('priority', 'asc')
+            ->where('parent_id', '<=', 0)
+            ->with('children')
+            ->get();
+
+        if ($categories) {
+
+            $cat = [];
+            foreach($categories as $category) {
+                $cat[$category->category] = $category->toArray();
+                $cat[$category->category]['children'] = $this->findChildren($category);
+            }
+            // var_dump($cat);
+            // $sql = DB::getQueryLog();
+            return response($cat, 200);
+
+        } else {
+            $response = null;
+            return response($response, 422);
+        }
+
+    }
+}
---
+++ setenv.bat
@@ -0,0 +1,1 @@
+set PATH=d:\apps\xampp\php;d:\Users\limkc\AppData\Roaming\Composer\vendor\bin;d:\apps\xampp\php;d:\Users\limkc\AppData\Roaming\Composer\vendor\bin;%PATH%
